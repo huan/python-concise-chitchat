535c9f444079cef40bb68607baf2a018130f0065
Huan LI <zixia@zixia.net>
2018-10-09 10:32:02 +0800

    fix download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -6,9 +6,9 @@ set -e
 (
     cd data
 
-    [ -e cornell_movie_dialogs_corpus.zip ] || curl -L -O -C - http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip
+    [ -e cornell_movie_dialogs_corpus.zip ] || curl -L -O http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip
     [ -d 'cornell movie-dialogs corpus' ]   || unzip -o cornell_movie_dialogs_corpus.zip
 
-    [ -e glove.6B.50d.txt.gz ]  || curl -L -O -C - https://github.com/zixia/concise-chit-chat/releases/download/v0.0.1/glove.6B.50d.txt.gz
+    [ -e glove.6B.50d.txt.gz ]  || curl -L -O https://github.com/zixia/concise-chit-chat/releases/download/v0.0.1/glove.6B.50d.txt.gz
     [ -e glove.6B.50d.txt ]     || gzip -d -f -k glove.6B.50d.txt.gz
 )
