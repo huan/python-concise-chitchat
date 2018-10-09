6b2922564da2ad9c1514bf4fee928f168997bb9c
Huan LI <zixia@zixia.net>
2018-10-09 10:42:36 +0800

    code clean

--- src/train.py
+++ src/train.py
@@ -11,15 +11,16 @@ import numpy as np
 import tensorflow as tf
 tf.enable_eager_execution()
 
-# load the data
-question_sequence = np.load('data/questions.npy')
-answer_sequence = np.load('data/answers.npy')
-
 with open('data/tokenizer.pkl', 'rb') as f:
     tokenizer: tf.keras.preprocessing.text.Tokenizer = pickle.load(f)
 
 VOCABULARY_SIZE = tokenizer.num_words
 MAX_LEN = 20
+LSTM_UNITS_NUM = 300
+
+# load the data
+question_sequence = np.load('data/questions.npy')
+answer_sequence = np.load('data/answers.npy')
 
 
 def load_glove_dict(file: str) -> Dict[str, np.array]:
@@ -36,12 +37,12 @@ def load_glove_dict(file: str) -> Dict[str, np.array]:
 
 
 glove_dict = load_glove_dict('data/glove.6B.50d.txt')
-GLOVE_DIMENTION = 50
+EMBEDDING_DIMENTION = 50
 
 print('got glove embedding for {} words'.format(len(glove_dict.keys())))
 # print('glove embedding for "...": {}'.format(glove_dict.get('...')))
 
-embedding_matrix = np.zeros((VOCABULARY_SIZE, GLOVE_DIMENTION))
+embedding_matrix = np.zeros((VOCABULARY_SIZE, EMBEDDING_DIMENTION))
 for index in range(1, tokenizer.num_words):
     word = tokenizer.index_word[index]
     # print('get glove embedding for: {}'.format(word))
@@ -60,14 +61,14 @@ for i, word_id_list in enumerate(answer_sequence):
     if i % 5000 == 0:
         print('{} entries completed'.format(i))
 
-padded_question_sentences = tf.keras.preprocessing.sequence.pad_sequences(
+padded_question_sequence = tf.keras.preprocessing.sequence.pad_sequences(
     question_sequence,
-    maxlen=20,
+    maxlen=MAX_LEN,
     dtype='int32',
     padding='post',
     truncating='post',
 )
-padded_answer_sentences = tf.keras.preprocessing.sequence.pad_sequences(
+padded_answer_sequence = tf.keras.preprocessing.sequence.pad_sequences(
     answer_sequence,
     maxlen=MAX_LEN,
     dtype='int32',
@@ -77,20 +78,20 @@ padded_answer_sentences = tf.keras.preprocessing.sequence.pad_sequences(
 
 embedding_layer = tf.keras.layers.Embedding(
     input_dim=VOCABULARY_SIZE,
-    output_dim=50,
+    output_dim=EMBEDDING_DIMENTION,
     trainable=False,
 )
 embedding_layer.build((None,))
 embedding_layer.set_weights([embedding_matrix])
 
 encoder_lstm_layer = tf.keras.layers.LSTM(
-    300,
+    units=LSTM_UNITS_NUM,
     dropout=0.1,
     recurrent_dropout=0.1,
     return_state=True,
 )
 decoder_lstm_layer = tf.keras.layers.LSTM(
-    300,
+    units=LSTM_UNITS_NUM,
     dropout=0.1,
     recurrent_dropout=0.1,
     return_state=True,
@@ -107,12 +108,12 @@ dense_layer = tf.keras.layers.TimeDistributed(
 question_input_layer = tf.keras.layers.Input(
     shape=(MAX_LEN, ),
     dtype='int32',
-    name='current_sentence',
+    name='question',
 )
 answer_input_layer = tf.keras.layers.Input(
     shape=(MAX_LEN, ),
     dtype='int32',
-    name='next_sentence',
+    name='answer',
 )
 
 question_embedding_layer = embedding_layer(question_input_layer)
@@ -130,14 +131,14 @@ decoded_state, _, _ = decoder_lstm_layer(
     ],
 )
 
-predict_output = dense_layer(decoded_state)
+decoded_output = dense_layer(decoded_state)
 
 model = tf.keras.Model(
     [
         question_input_layer,
         answer_input_layer
     ],
-    predict_output,
+    decoded_output,
 )
 
 adam = tf.train.AdamOptimizer()
@@ -155,11 +156,11 @@ model.compile(
 
 print('Start training ...')
 model.fit(
-    [
-        padded_question_sentences,
-        padded_answer_sentences,
+    x=[
+        padded_question_sequence,
+        padded_answer_sequence,
     ],
-    teacher_forcing_answers,
+    y=teacher_forcing_answers,
     epochs=10,
     batch_size=16,
 )
